97f6c61b48639f3b976607a8e2beb7e83f108f58
Lsy322 <j3a2s2o2n8@gmail.com>
2022-03-02 00:31:12 +0800

    Added Controllers

---
+++ backend/controllers/customer.js
@@ -0,0 +1,13 @@
+const customerCollection = require("../models/customer.js")
+
+module.exports = {
+    getCustomer : async (req,res) => {
+        //TODO : Fetch user from database
+    },
+
+    addCustomer : (req,res) => {
+        //TODO : Add Customer to database (Register)
+    }  
+
+
+}
