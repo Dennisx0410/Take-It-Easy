2b5c7591ab4ecea4237ea98a0c979b66cfa868eb
unknown <1155127407@link.cuhk.edu.hk>
2022-03-02 00:35:46 +0800

    backend hello world

--- backend/index.js
+++ backend/index.js
@@ -10,3 +10,8 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology: true})
 .then(()=> app.listen(PORT, () => console.log('Server running on port ' + PORT)))
 .catch((err) => console.log(err));
+
+
+app.get('/', (req, res) => {
+  res.send('hello world take it easy')
+})
