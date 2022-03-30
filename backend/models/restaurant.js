7227095ea5276d53b106ecd3e28be9806f7f97b3
Lsy322 <j3a2s2o2n8@gmail.com>
2022-03-30 23:44:45 +0800

    Changed Restaurant Schema

--- backend/models/restaurant.js
+++ backend/models/restaurant.js
@@ -15,5 +15,29 @@ const restaurantSchema = new Schema({
     menu : [Object]
 });
 
+restaurantSchema.pre('save', async function (next) {
+    console.log('save action detected, check changes');
+    const restaurant = this;
+  
+    // console.log('orig pw: ', restaurant.password);
+  
+    if (restaurant.isModified('password')) {
+      console.log('password changed, hash before saving');
+      restaurant.password = await bcrypt.hash(restaurant.password, SALTLEN);
+    }
+    next();
+  })
+  
+  // instance method for generating jwt token
+  restaurantSchema.methods.genAuthToken = async function () {
+    console.log('> generating auth token');
+    let restaurant = this;
+    // assume never expire
+    // let token = jwt.sign({_id: restaurant._id.toString()}, process.env.SECRET);
+    let token = jwt.sign({_id: restaurant._id.toString()}, process.env.SECRET, {expiresIn: EXPIRE});
+    console.log('> generated token');
+    return token;
+  }
+
 const Restaurants = mongoose.model('Restaurants', restaurantSchema);
 module.exports = Restaurants;
